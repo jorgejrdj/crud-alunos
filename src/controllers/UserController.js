38b25d6859f34822a344307b9afafa9d61a56898
Jorge de Souza <jorgejrdj1@gmail.com>
2021-01-27 06:52:30 -0300

    CRUD dos Usuários - terminado

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -11,16 +11,81 @@ class UserController {
       });
     }
   }
+
+  // index -> lista todos os usuários -> GET
   async index(req, res) {
     try {
       const users = await User.findAll();
-      return res.json();
+      return res.json(users);
     }
     catch (e) {
       return res.json(null);
     }
   }
 
+  //show -> mostra um usuário -> GET
+  async show(req, res) {
+    try {
+      const users = await User.findByPk(req.params.id);
+      return res.json(users);
+    }
+    catch (e) {
+      return res.json(null);
+    }
+  }
+
+  //update -> atualiza um usuário -> PATCH ou PUT
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['Id não enviado']
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe'],
+        });
+      }
+      const novosDados = await user.update(req.body);
+      return res.json(novosDados);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+
+  }
+  //delete -> apaga um usuário -> DELETE
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['Id não enviado']
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe'],
+        });
+      }
+
+      await user.destroy;
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
 }
+//store/create -> cria um novo usuário -> POST
 
 export default new UserController();
