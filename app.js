d05aef6519b9c997d0d99bac7f5608f18a8523d8
Jorge de Souza <jorgejrdj1@gmail.com>
2021-03-10 13:17:17 -0300

    uploads de arquivos

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import homeRoutes from './src/routes/homeRoutes';
 import userRoutes from './src/routes/userRoutes';
 import tokenRoutes from './src/routes/tokenRoutes';
 import alunoRoutes from './src/routes/alunoRoutes';
+import fotoRoutes from './src/routes/fotoRoutes';
 
 import './src/database';
 
@@ -24,9 +25,10 @@ class App {
 
   routes() {
     this.app.use('/', homeRoutes);
-    this.app.use('/users', userRoutes);
-    this.app.use('/tokens', tokenRoutes);
-    this.app.use('/alunos', alunoRoutes);
+    this.app.use('/users/', userRoutes);
+    this.app.use('/tokens/', tokenRoutes);
+    this.app.use('/alunos/', alunoRoutes);
+    this.app.use('/fotos/', fotoRoutes);
   }
 }
 
