df65c801c5f00f5e71914d58fe5a0b3ecde4078b
Jorge de Souza <jorgejrdj1@gmail.com>
2021-03-10 14:34:24 -0300

    sALVANDO AS FOTOS NO DB - SEGISTROS

--- src/database/migrations/20210310012850-mudar-email-aluno-unique.js
+++ src/database/migrations/20210310012850-mudar-email-aluno-unique.js
@@ -1,15 +1,38 @@
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.changeColumn(
-      'alunos',
-      'email',
-      {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
+  up: (queryInterface, Sequelize) => queryInterface.createTable('fotos', {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    originalname: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    filename: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    aluno_id: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'alunos',
+        key: 'id',
       },
-    );
-  },
+      onDelete: 'SET NULL',
+      ONuPDATE: 'CASCADE',
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  }),
 
-  down: () => { },
+  down: (queryInterface) => queryInterface.dropTable('fotos'),
 };
