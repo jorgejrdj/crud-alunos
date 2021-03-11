cf6c6912ae79966efd62797165eec44e1d64a246
Jorge de Souza <jorgejrdj1@gmail.com>
2021-03-10 21:07:50 -0300

    reatualizando - part 1

--- dist/server.js
+++ dist/server.js
@@ -1,9 +1,5 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
 
-const port = 3001;
+const port = process.env.APP_PORT;
 
-_app2.default.listen(port, () => {
-  console.log();
-  console.log(`Escutando na port ${port}`);
-  console.log(`CTRL + Clique em http://localhost:${port}`);
-});
+_app2.default.listen(port);
